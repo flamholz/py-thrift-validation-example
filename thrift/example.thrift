83c9686ed3ca61e5c7be00418726b2afdfe2686e
Avi Flamholz <flamholz@ridewithvia.com>
2012-12-03 10:41:43 -0500

    Add a Review structure.

--- thrift/example.thrift
+++ thrift/example.thrift
@@ -16,10 +16,14 @@ struct Point {
 	2: required double y;
 }
 
+struct Review {
+	1: required i32 rating;
+	2: optional string text;
+}
 
 struct Place {
 	1: required string name;
 	2: required Point location;
-	3: optional string review;
+	3: optional Review review;
 }
 
